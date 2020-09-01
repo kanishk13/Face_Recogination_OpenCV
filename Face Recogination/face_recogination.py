ac4bcbe2bbffe64ffb85adee58621ccb2bb00a81
kanishk13 <42895589+kanishk13@users.noreply.github.com>
2020-09-01 21:36:11 +0530

    Update face_recogination.py

--- Face Recogination/face_recogination.py
+++ Face Recogination/face_recogination.py
@@ -1,35 +1,24 @@
-#Recognise Face using Classification Algorithm -KNN 
-
-# 1.Read A Video Stream using OpenCV
-# 2.Extract faces out of it
-# 3.Load Training data (numpy array of all the persons )
-	# x-values are stored in the numpy arrays 
-	# y-values we need to assign for each person 
-#4. use knn to find the prediction to face (int)
-#5.map to predicit id to the name of the user 
-#6. Display the predicitions on the screen - bounding box and name 
-
 import cv2
 import numpy as np
 import os 
 
 
-def distance(v1,v2):
-	return np.sqrt(((v1-v2)**2).sum())
+def distance(v1,v2):                    #takes two vector vector v1 ma
+	return np.sqrt(((v1-v2)**2).sum())  #Euclidean distance formula 
 		
-def knn(train,test,k=5):
-	dist=[]
+def knn(train,test,k=5):   		 # 5 query points 
+	dist=[]                      # distance array  
 	
-	for i in range(train.shape[0]):
-		ix = train[i, :-1]
+	for i in range(train.shape[0]):       
+		ix = train[i, :-1]            # point in the dataset 
 		iy =train[i, -1]
-		d =distance(test,ix)
-		dist.append([d,iy])
+		d =distance(test,ix)        
+		dist.append([d,iy])        
 	
-	dk=sorted(dist,key=lambda x:x[0])[:k]
+	dk=sorted(dist,key=lambda x:x[0])[:k]     # sort according the distance 
 	labels =np.array(dk)[:,-1]
 	output=np.unique(labels,return_counts=True)
-	index=np.argmax(output[1])
+	index=np.argmax(output[1])             
 	
 	return output[0][index]
 
@@ -37,47 +26,49 @@ def knn(train,test,k=5):
 
 cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 
-
+#to extract the faces 
 face_cascade = cv2.CascadeClassifier("C:\\Users\\kanishk vohra\\Desktop\\Face Recogination\\haarcascade_frontalface_alt.xml")
 
 skip = 0
 dataset_path ='C:\\Users\\kanishk vohra\\Desktop\\Face Recogination\\data\\'  
 
-face_data = []
-labels=[]
+face_data = []          # training data load all the files x of the data 
+labels=[]				# y values of our data 
 
-class_id = 0 
+class_id = 0            # file one will be intlized with zero  lables for tha given file 
 names={} 
 
-for fx in os.listdir(dataset_path):
-	if fx.endswith('.npy'):
-		names[class_id]=fx[:-4]
+#data prepration 
+
+for fx in os.listdir(dataset_path):    # all the files in folder path 
+	if fx.endswith('.npy'):            # if your file ends with numpy file 
+		names[class_id]=fx[:-4]        # remove the last four character to get the name of the file ex kanishk.npy        
 		print("loaded "+fx)
-		data_item = np.load(dataset_path+fx)
-		face_data.append(data_item)
-		target = class_id*np.ones((data_item.shape[0],))
-		class_id+=1
-		labels.append(target)
-	
+		data_item = np.load(dataset_path+fx)  	# file name along with the name 
+		face_data.append(data_item)			 	# faces list
+		target = class_id*np.ones((data_item.shape[0],))    #
+		class_id+=1    							# incrementing the class id by one 
+		labels.append(target)					#computing the lablesa and saving it     
+												
 face_dataset = np.concatenate(face_data,axis=0)
 face_lables=np.concatenate(labels,axis=0).reshape((-1,1))	
 
-print(face_dataset.shape)
-print(face_lables.shape)
+print(face_dataset.shape)      #(faces,features)
+print(face_lables.shape) 		
 
-trainset = np.concatenate((face_dataset,face_lables),axis=1)
-print(trainset.shape)
+trainset = np.concatenate((face_dataset,face_lables),axis=1)  	
+print(trainset.shape)   										
 
 
 while True:
-	ret,frame= cap.read()
-	if ret== False:
+	ret,f rame= cap.read()
+	if ret== False:      # if we can't detect the face  then recapture 
 		continue
-	
-	faces = face_cascade.detectMultiScale(frame,1.3,5)
+						# if we detected the face 								
+	faces = face_cascade.detectMultiScale(frame,1.3,5)   # giving the frame scaling 
 	
 	for face in faces:
-		x,y,w,h =face
+		x,y,w,h =face        
 		offset = 10 
 		face_section = frame[y-offset:y+h+offset,x-offset:x+w+offset]
 		face_section = cv2.resize(face_section,(100,100))
