20fb9f56f8b759417ebd157302e5cfd7f6ba2bca
kanishk13 <42895589+kanishk13@users.noreply.github.com>
2020-09-01 20:50:28 +0530

    Update face_recogination.py

--- Face Recogination/face_recogination.py
+++ Face Recogination/face_recogination.py
@@ -1,3 +1,14 @@
+#Recognise Face using Classification Algorithm -KNN 
+
+# 1.Read A Video Stream using OpenCV
+# 2.Extract faces out of it
+# 3.Load Training data (numpy array of all the persons )
+	# x-values are stored in the numpy arrays 
+	# y-values we need to assign for each person 
+#4. use knn to find the prediction to face (int)
+#5.map to predicit id to the name of the user 
+#6. Display the predicitions on the screen - bounding box and name 
+
 import cv2
 import numpy as np
 import os 
